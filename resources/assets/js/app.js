a2a13da6aa986e4fce572e505597a804ca77e85e
nickclasener <nick.clasener@gmail.com>
2018-05-16 20:32:38 +0200

    stashing blade

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,12 +6,12 @@
 require('./bootstrap');
 
 
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-
-const application = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
+// import { Application } from "stimulus";
+// import { definitionsFromContext } from "stimulus/webpack-helpers";
+//
+// const application = Application.start();
+// const context = require.context("./controllers", true, /\.js$/);
+// application.load(definitionsFromContext(context));
 
 // Rails.start();
 Turbolinks.start();
